bfabbdf9ed5189dc8034e717768cbbd211c9ccea
“dlgilligan” <danny.gilligan26@gmail>
2022-06-19 15:59:20 -0400

    Added Comments

--- models/author.js
+++ models/author.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // Imports mongoose
 
-const authorSchema = new mongoose.Schema( {
+const authorSchema = new mongoose.Schema( { //Creates new schema with JSON name
     name: {
         type: String,
         required: true
@@ -8,3 +8,6 @@ const authorSchema = new mongoose.Schema( {
 })
 
 module.exports = mongoose.model('Author', authorSchema)
+// Exports model
+// 'Author" is the name of the model or what it is for 
+// authorSchema is the Schema we created above
--- routes/authors.js
+++ routes/authors.js
@@ -1,44 +1,52 @@
 const express = require('express')
-const Author = require('../models/author')
+const Author = require('../models/author') // Navigate out of routes folder and into models folder 
 const router = express.Router()
 
-// All Authors Route
+// All Authors Route  // Mongoose is async, so add async to beginning of function and use async when referencing mongoose
 router.get('/', async (req,res) => {
-    let searchOptions = {}
+    let searchOptions = {} // Empty object created to contain search options 
+
+    // If the query.name submitted is not empty or null 
     if (req.query.name != null && req.query.name !== '') { // Use req.query, GET sends through query, POST through body
-        searchOptions.name = new RegExp(req.query.name, 'i')
+
+        // Appends RegExp object to searchOptions
+        searchOptions.name = new RegExp(req.query.name, 'i') // Creates a regular expression object for matching text with a pattern 
+        // RegExp(pattern, flags), pattern is text, flags are for seach (i being ignore case)
+    
     }
     try {
-        const authors = await Author.find(searchOptions)
+        const authors = await Author.find(searchOptions) // Use searchOptions to match text stored in model
         res.render('authors/index', {
-            authors: authors, 
-            searchOptions: req.query
+            authors: authors, // render results from search 
+            searchOptions: req.query // Keep search in search bar
         })
     } catch {
-        res.redirect('/')
+        res.redirect('/') // Redirect back to homepage if anything fails
     }
 })
 
 // New Author Route
 router.get('/new', (req, res) => {
-    res.render('authors/new', { author: new Author()})
+    res.render('authors/new', { author: new Author()}) // Define Author for the view 
 })
 
 // Create Author Route
 router.post('/', async (req, res) => {
-    const author = new Author( {
+    const author = new Author( { // Create new Author variable with name from POST
         name: req.body.name
     })
 
     try {
-        const newAuthor = await author.save()
+        const newAuthor = await author.save() // Save author in model
         // res.redirect(`authors/${newAuthor.id}`)
         res.redirect(`authors/`)
     } catch {
-        res.render('authors/new', {
+        res.render('authors/new', { // Display error message and keep author in bar 
             author: author,
             errorMessage: "Error Creating Author"
         })
     }
 })
+
+
 module.exports = router
